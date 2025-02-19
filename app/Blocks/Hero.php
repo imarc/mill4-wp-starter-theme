2cd3460c5d36e3a6f94c8a5ce5fed515785508ca
Dave Anastasi <daveanastasi@imarc.com>
2025-02-19 16:58:47 -0500

    Updates hero block to untether itself from pronto

--- app/Blocks/Hero.php
+++ app/Blocks/Hero.php
@@ -7,7 +7,6 @@ class Hero extends Block
     public const NAME = 'hero';
     public const TITLE = 'Hero';
     public const CATEGORY = 'section';
-    public const ICON = 'admin-comments';
+    public const ICON = 'cover-image';
     public const POST_TYPES = [];
-    public const KEYWORDS = ['pronto', 'hero'];
 }
