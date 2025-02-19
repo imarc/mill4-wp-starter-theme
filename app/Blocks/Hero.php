54fea3453eec7ef32e3886eb5eb6097c0e8816b4
Dave Anastasi <daveanastasi@imarc.com>
2025-02-18 21:30:48 -0500

    Basic hero block and template, using pronto markup

---
+++ app/Blocks/Hero.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Blocks;
+
+class Hero extends Block
+{
+    public const NAME = 'hero';
+    public const TITLE = 'Hero';
+    public const CATEGORY = 'section';
+    public const ICON = 'admin-comments';
+    public const POST_TYPES = [];
+    public const KEYWORDS = ['pronto', 'hero'];
+}
