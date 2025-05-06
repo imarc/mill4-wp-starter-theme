8969dbd683bb58c77cbfef3267ed36690b224bd7
Dave Anastasi <daveanastasi@imarc.com>
2025-05-06 15:55:55 -0400

    Disables commenting by default.

--- app/Hooks/ThemeHooks.php
+++ app/Hooks/ThemeHooks.php
@@ -21,6 +21,7 @@ class ThemeHooks implements HooksInterface
         $this->addHtml5Support();
         $this->addPostFormatSupport();
         $this->addMenuSupport();
+        $this->disableCommenting();
     }
 
     private function addBasicThemeSupports(): void
@@ -112,5 +113,85 @@ class ThemeHooks implements HooksInterface
             $locations[$location] = $menuId;
             set_theme_mod('nav_menu_locations', $locations);
         }
+    }
+
+    private function disableCommenting(): void
+    {
+        // Disable support for comments and trackbacks in post types
+        $this->addAction('init', function () {
+            foreach (get_post_types() as $post_type) {
+                if (post_type_supports($post_type, 'comments')) {
+                    remove_post_type_support($post_type, 'comments');
+                    remove_post_type_support($post_type, 'trackbacks');
+                }
+            }
+        });
+
+        // Remove Comment Support from Admin Post Editor
+        $this->addAction('admin_init', function () {
+            foreach (get_post_types() as $post_type) {
+                if (post_type_supports($post_type, 'comments')) {
+                    remove_post_type_support($post_type, 'comments');
+                    remove_post_type_support($post_type, 'trackbacks');
+                }
+            }
+        });
+
+        // Close existing comments
+        $this->addFilter('comments_open', '__return_false', 20, 2);
+        $this->addFilter('pings_open', '__return_false', 20, 2);
+
+        // Remove comments from admin UI
+        $this->addAction('admin_menu', function () {
+            remove_menu_page('edit-comments.php');
+        });
+
+        $this->addAction('admin_menu', function () {
+            remove_submenu_page('options-general.php', 'options-discussion.php');
+        });
+
+        $this->addAction('admin_bar_menu', function ($wp_admin_bar) {
+            $wp_admin_bar->remove_node('comments');
+        }, 999);
+
+        $this->addAction('wp_before_admin_bar_render', function () {
+            global $wp_admin_bar;
+            $wp_admin_bar->remove_menu('comments');
+        });
+
+        // hide comments metabox on dashboard
+        $this->addAction('wp_dashboard_setup', function () {
+            remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+        });
+
+        // Remove comments template output
+        $this->addFilter('comments_template', '__return_false', 20, 1);
+
+        // Remove comment RSS feed.
+        $this->removeAction('wp_head', 'feed_links_extra', 3);
+        $this->addAction('do_feed_rdf', '__return_false', 1);
+        $this->addAction('do_feed_rss', '__return_false', 1);
+        $this->addAction('do_feed_rss2', '__return_false', 1);
+        $this->addAction('do_feed_atom', '__return_false', 1);
+        $this->addAction('do_feed_rss2_comments', '__return_false', 1);
+        $this->addAction('do_feed_atom_comments', '__return_false', 1);
+
+        // Block access to comment submission.
+        $this->addAction('pre_comment_on_post', function () {
+            wp_die('Comments are closed.', 'Comments Closed', ['response' => 403]);
+        });
+
+        // Disable pingbacks and trackbacks
+        $this->addFilter('xmlrpc_methods', function ($methods) {
+            unset($methods['pingback.ping']);
+            return $methods;
+        });
+        $this->addFilter('pre_option_default_ping_status', '__return_false');
+        $this->addFilter('pre_option_default_comment_status', '__return_false');
+
+        // Remove comment-related widgets
+        $this->addAction('widgets_init', function () {
+            unregister_widget('WP_Widget_Recent_Comments');
+        });
     }
 }
