c1bdb3b7d31999ae9d48d79cb469340dd975c87d
Dave Anastasi <daveanastasi@imarc.com>
2025-12-11 11:06:52 -0500

    cs fix

--- app/Hooks/ThemeHooks.php
+++ app/Hooks/ThemeHooks.php
@@ -205,6 +205,7 @@ class ThemeHooks implements HooksInterface
     {
         add_filter('upload_mimes', function ($mimes) {
             $mimes['svg'] = 'image/svg+xml';
+
             return $mimes;
         });
     }
